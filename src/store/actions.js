feb7a2fa9aba04796b7faa9151e9afae876b5d47
Vocter <ameliepoulain-92@mail.ru>
2017-07-31 15:07:12 +0300

    add actions.js to the store

---
+++ src/store/actions.js
@@ -0,0 +1,14 @@
+import api from '../api'
+
+export const getUsers = ({ commit }, payload) => {
+  const urlEnd = '/users'
+  const type = 'post'
+  // replace below method to "requestToServer"
+  api.requestUsers(urlEnd, type, payload)
+    .then((res) => {
+      commit('setUsers', res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
