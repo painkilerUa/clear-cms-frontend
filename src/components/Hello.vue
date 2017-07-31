be87fdab27cae39c82363f0066a0ee987bc4956e
Vocter <ameliepoulain-92@mail.ru>
2017-07-31 15:30:13 +0300

    show users

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="hello">
+	{{users}}
+
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est consequatur commodi laboriosam officia assumenda impedit. Reprehenderit saepe, hic, nobis ut, odio ex harum iure rem repellendus voluptatibus cumque id odit.
   </div>
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+
 export default {
   name: 'hello',
   data () {
     return {
     }
+  },
+  methods: {
+    ...mapActions([
+      'getUsers'
+    ])
+  },
+  mounted () {
+    this.getUsers()
   }
 }
 </script>
