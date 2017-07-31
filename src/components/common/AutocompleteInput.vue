7b948c3a0e3994869305e1a567ea6dc1c3db782d
Vocter <ameliepoulain-92@mail.ru>
2017-08-01 00:18:09 +0300

    add autocomplete input skeleton

---
+++ src/components/common/AutocompleteInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="autocomplete-input">
+    <p class="control has-icon has-icon-right">
+      <input class="input is-large" placeholder="Search..." />
+    </p>
+    <ul class="options-list">
+      <li v-for="option in options">
+        {{ option.title }}
+      </li>
+    </ul>
+  </div>
+</template>
