130f94ca9332d59ab8a81ffe6821606af320cf02
Vocter <ameliepoulain-92@mail.ru>
2017-07-31 17:34:06 +0300

    minimize actions.js from store

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,12 +5,8 @@ export const getContent = ({ commit }, payload) => {
   const type = 'post'
   // replace below method to "requestToServer"
   api.requestContent(urlEnd, type, payload)
-    .then((res) => {
-      commit('setContent', res)
-    })
-    .catch((err) => {
-      console.error(err)
-    })
+    .then((res) => commit('setContent', res))
+    .catch((err) => console.error(err))
 }
 
 export const getUsers = ({ commit }, payload) => {
@@ -18,10 +14,6 @@ export const getUsers = ({ commit }, payload) => {
   const type = 'post'
   // replace below method to "requestToServer"
   api.requestUsers(urlEnd, type, payload)
-    .then((res) => {
-      commit('setUsers', res)
-    })
-    .catch((err) => {
-      console.error(err)
-    })
+    .then((res) => commit('setUsers', res))
+    .catch((err) => console.error(err))
 }
