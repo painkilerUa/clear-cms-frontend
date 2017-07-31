600553f2fd133be3c1014a6a364feac0a54ab278
Vocter <ameliepoulain-92@mail.ru>
2017-07-31 16:39:57 +0300

    fix comma

--- src/api/index.js
+++ src/api/index.js
@@ -14,31 +14,31 @@ const users = [
   {
     id: 1,
     name: 'User 1',
-    role: 'Admin'
+    role: 'Admin',
     date: '2017-06-29 12:09:33'
   },
   {
     id: 2,
     name: 'User 2',
-    role: 'Admin'
+    role: 'Admin',
     date: '2017-06-29 12:09:33'
   },
   {
     id: 3,
     name: 'User 3',
-    role: 'Admin'
+    role: 'Admin',
     date: '2017-06-29 12:09:33'
   },
   {
     id: 4,
     name: 'User 4',
-    role: 'Admin'
+    role: 'Admin',
     date: '2017-06-29 12:09:33'
   },
   {
     id: 5,
     name: 'User 5',
-    role: 'Admin'
+    role: 'Admin',
     date: '2017-06-29 12:09:33'
   }
 ]
