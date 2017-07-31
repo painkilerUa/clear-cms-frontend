0bec379db720a4a3c94f66ba7fbc084655b378fb
Vocter <ameliepoulain-92@mail.ru>
2017-07-31 14:53:07 +0300

    add store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+/**
+ * App state storage
+ */
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as getters from './getters'
+import * as actions from './actions'
+import * as mutations from './mutations'
+Vue.use(Vuex)
+const state = {
+  users: [],
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
