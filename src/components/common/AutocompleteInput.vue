7e1ecc91f5d863f323a29b05e114c99aff423086
Vocter <ameliepoulain-92@mail.ru>
2017-08-01 10:04:43 +0300

    insert slots props autocomplete

--- src/components/common/AutocompleteInput.vue
+++ src/components/common/AutocompleteInput.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="autocomplete-input">
     <input class="ac-input" placeholder="Search..." />
-    <!-- <ul class="options-list">
+    <ul class="options-list">
       <li v-for="option in options">
-        {{option}}
+        <slot name="item" 
+          :title="option.title" 
+          :description="option.description" 
+          :thumbnail="  option.thumbnail" />
       </li>
-    </ul> -->
-    <slot name="item" :title="option.title" :description="option.description" :thumbnail="option.thumbnail" />
+    </ul>
   </div>
 </template>
 
