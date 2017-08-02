70a861fba4ceefe14ce23ff3bbdbd308654f3b94
Vocter <ameliepoulain-92@mail.ru>
2017-08-03 00:13:24 +0300

    add additional comp

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -6,8 +6,8 @@
       <img v-if="option.thumbnail" :src="option.thumbnail" :alt="option.title" class="ac-thumbnail" />
       <!-- .ac-content -->
       <div class="ac-content">
-        <h3 class="ac-title">{{option.title}}</h3>
-        <div class="ac-description">{{option.description}}</div>
+        <h3 class="ac-title" v-if="option.title">{{option.title}}</h3>
+        <div class="ac-description" v-if="option.description">{{option.description}}</div>
       </div>
       <!-- END:.ac-content -->
     </template>
