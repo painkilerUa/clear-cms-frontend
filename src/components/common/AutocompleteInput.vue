e11071d85326554ca79073a1bc081f7e48cd5a4b
Vocter <ameliepoulain-92@mail.ru>
2017-08-02 23:43:23 +0300

    add velocity lib

--- src/components/common/AutocompleteInput.vue
+++ src/components/common/AutocompleteInput.vue
@@ -17,9 +17,17 @@
         </div>
         <!-- END:.ac-filters -->
         <!-- ac-list -->
-        <ul class="ac-list">
+        <transition-group
+        tag="ul"
+        name="staggered-fade"
+        class="ac-list">
           <li v-for="(option, index) in optionsFilteredLimited" 
+            :key="option.title"
             class="ac-list__item"
+            :css="false"
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @leave="leave"
             :class="{highlighted: index === highlightedPosition}"
             @mouseenter="highlightedPosition = index"
             @mousedown="select"> 
@@ -28,7 +36,7 @@
               :description="option.description" 
               :thumbnail="option.thumbnail" />
           </li>
-        </ul>
+        </transition-group>
         <!-- END:ac-list -->
         <button type="button" v-if="optionsLeft.length" class="ac-more" @mousedown="addLeftOptions">+ See more</button>
       </div>
@@ -39,6 +47,8 @@
 </template>
 
 <script>
+import Velocity from 'velocity-animate'
+
 export default {
   props: {
     options: {
@@ -104,7 +114,34 @@ export default {
     addLeftOptions () {
       this.limitToShow += 2
       this.optionsToShow.concat(this.optionsLeftShow)
+    },
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+      el.style.height = 0
+    },
+    enter: function (el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 1, height: '1.6em' },
+          { complete: done }
+        )
+      }, delay)
+    },
+    leave: function (el, done) {
+      var delay = el.dataset.index * 150
+      setTimeout(function () {
+        Velocity(
+          el,
+          { opacity: 0, height: 0 },
+          { complete: done }
+        )
+      }, delay)
     }
+  },
+  components: {
+    Velocity
   }
 }
 </script>
