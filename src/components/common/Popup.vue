a2d1fafb6763e7856252fbb37e4136c398cf60ba
Vocter <ameliepoulain-92@mail.ru>
2017-08-08 17:48:49 +0300

    add new popups

---
+++ src/components/common/Popup.vue
@@ -0,0 +1,19 @@
+<template>
+	<!-- .popup -->
+	<div class="popup">
+		<!-- .popup__inner -->
+		<div class="popup__inner">
+			<slot name="popup-inner"></slot>
+		</div>
+		<!-- END:.popup__inner -->
+	</div>
+	<!-- END:.popup -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="@/assets/scss/components/popup" lang="scss" scoped/>
