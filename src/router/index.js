0d2f3ed01ac21561fa16c53e4ce536076a097ee7
Vocter <ameliepoulain-92@mail.ru>
2017-08-08 17:16:17 +0300

    remove users

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Users from '@/components/Users'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'users',
-      component: Users
+      path: '/'
     }
   ]
 })
