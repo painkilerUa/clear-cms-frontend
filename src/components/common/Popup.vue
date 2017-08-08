9f14ca69ba0ae6a39f4298d2e3d7bc8038445a0c
Vocter <ameliepoulain-92@mail.ru>
2017-08-08 18:00:37 +0300

    modify popup

--- src/components/common/Popup.vue
+++ src/components/common/Popup.vue
@@ -3,7 +3,11 @@
 	<div class="popup">
 		<!-- .popup__inner -->
 		<div class="popup__inner">
-			<slot name="popup-inner" />
+			<h2 class="popup-title">{{title}}</h2>
+			<div class="popup-description">
+				<p>{{description}}</p>
+			</div>
+			<slot name="body"/>
 		</div>
 		<!-- END:.popup__inner -->
 	</div>
@@ -12,7 +16,17 @@
 
 <script>
 export default {
-
+ name: 'popup',
+ props: {
+   title: {
+     type: String,
+     default: 'Title'
+   },
+   description: {
+   	type: String,
+   	default: ''
+   }
+ }
 }
 </script>
 
