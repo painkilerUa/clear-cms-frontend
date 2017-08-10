1d57b86344e030352a8c4b36cf9bb9250eaee75b
Vocter <ameliepoulain-92@mail.ru>
2017-08-11 00:39:48 +0300

    remove animations

--- src/components/common/AutocompleteInput.vue
+++ src/components/common/AutocompleteInput.vue
@@ -63,7 +63,6 @@
 <script>
 import api from '@/api'
 import { mapGetters, mapActions } from 'vuex'
-import Velocity from 'velocity-animate'
 import { directive as onClickaway } from 'vue-clickaway'
 
 export default {
@@ -165,37 +164,10 @@ export default {
     selectTags (val) {
       this.selectedTags = this.getTagIdByNames(val)
       this.fetchContent()
-    },
-    beforeEnter: function (el) {
-      el.style.opacity = 0
-      el.style.height = 0
-    },
-    enter: function (el, done) {
-      var delay = el.dataset.index * 150
-      setTimeout(function () {
-        Velocity(
-          el,
-          { opacity: 1, height: '3.6em' },
-          { complete: done }
-        )
-      }, delay)
-    },
-    leave: function (el, done) {
-      var delay = el.dataset.index * 150
-      setTimeout(function () {
-        Velocity(
-          el,
-          { opacity: 0, height: 0 },
-          { complete: done }
-        )
-      }, delay)
     }
   },
   directives: {
     onClickaway
-  },
-  components: {
-    Velocity
   },
   mounted () {
     this.getTypes()
