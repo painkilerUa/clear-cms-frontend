fdaf08e99392225412bffdbca6e77f2794e64b1f
Vocter <ameliepoulain-92@mail.ru>
2017-08-10 15:22:44 +0300

    add articles ids

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,9 @@
 export const content = state => state.content
+export const types = state => state.types
+export const tags = state => state.tags
+
+// types
+export const getContentTypeTitles = (state, getters) => state.types.map(item => item.type)
+
+// tags
+export const getTagTitles = (state, getters) => state.tags.map(item => item.title)
