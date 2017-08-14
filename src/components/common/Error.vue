136c116bcf5108e7ce59f5d4970e0cb89f6ba2c9
Vocter <ameliepoulain-92@mail.ru>
2017-08-14 15:43:29 +0300

    change router

---
+++ src/components/common/Error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error">
+    <!-- error-wrapper -->
+    <div class="error-wrapper">
+      <h1>{{msg}}</h1>
+      <router-link class="app-navlist__link" :to="{name: 'home'}">go Home</router-link>
+    </div>
+    <!-- END:error-wrapper -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  data () {
+    return {
+      msg: 'Sorry, The Page is not exists'
+    }
+  }
+}
+</script>
+
+<style src="" lang="scss" scoped />
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HeaderLogin from '@/components/common/HeaderLogin'
+import Error from '@/components/common/Error'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      path: '/',
+      name: 'home'
+    },
+    {
+      path: '/header-login',
+      name: 'header-login',
+      component: HeaderLogin
+    },
+    {
+      path: '/error',
+      component: Error
     }
   ]
 })
