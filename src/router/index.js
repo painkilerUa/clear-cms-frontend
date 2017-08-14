017385b6b21793b79731db67a141d26f3405481a
Vocter <ameliepoulain-92@mail.ru>
2017-08-14 15:47:55 +0300

    home login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HeaderLogin from '@/components/common/HeaderLogin'
+import HomeLogin from '@/components/common/HomeLogin'
 import Error from '@/components/common/Error'
 
 Vue.use(Router)
@@ -12,9 +12,9 @@ export default new Router({
       name: 'home'
     },
     {
-      path: '/header-login',
-      name: 'header-login',
-      component: HeaderLogin
+      path: '/home-login',
+      name: 'home-login',
+      component: HomeLogin
     },
     {
       path: '/error',
