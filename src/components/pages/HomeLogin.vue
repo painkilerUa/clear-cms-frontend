17ed869fc5db4a733204e2ea4998af298da1688d
Vocter <ameliepoulain-92@mail.ru>
2017-08-15 07:30:17 +0300

    add login popup

--- src/components/pages/HomeLogin.vue
+++ src/components/pages/HomeLogin.vue
@@ -1,7 +1,15 @@
 <template>
 	<div class="home-login">
-		<popup :title="'Login to your account'">
-			
+		<popup :title="'Login to your account'" :modal="true">
+			<template slot="body">
+			  <!-- .form -->
+			  <form action="#" class="form">
+			  	<input type="text" class="form-control" placeholder="User Name" />
+			  	<input type="password" class="form-control" placeholder="Password" />
+			  	<button type="submit" class="form-control form-submit">Login</button>
+			  </form>
+			  <!-- END:.form -->
+			</template>
 		</popup>
 	</div>
 </template>
