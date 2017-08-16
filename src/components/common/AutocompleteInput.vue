d957c4e969c0101e9ff39ae6fd805947a6d39bd7
Vocter <ameliepoulain-92@mail.ru>
2017-08-16 16:19:54 +0300

    add new func for select

--- src/components/common/AutocompleteInput.vue
+++ src/components/common/AutocompleteInput.vue
@@ -5,7 +5,6 @@
       v-model.trim="searchQuery"
       placeholder="Search..."
       @input="onInput($event.target.value)"
-      @keydown.enter="select"
       @keyup.esc="close"
       @keydown.up="moveUp"
       @keydown.down="moveDown" />
@@ -34,16 +33,17 @@
           <li v-for="(option, index) in optionsComputed" 
             class="ac-list__item"
             :class="{highlighted: index === highlightedPosition}"
-            @mouseenter="highlightedPosition = index"
-            @mousedown="select"
-            @keydown.space.enter="select" tabindex="0"> 
-            <img v-if="option.content_type.icon" :src="option.content_type.icon" :alt="option.title" class="ac-thumbnail" />
-            <!-- .ac-content -->
-            <div class="ac-content">
-              <h3 class="ac-title" v-if="option.title">{{option.title}}</h3>
-              <div class="ac-description" v-if="option.description">{{option.description}}</div>
-            </div>
-            <!-- END:.ac-content -->
+            @mouseenter="highlightedPosition = index" 
+            tabindex="0"> 
+            <router-link :to="{name: 'login'}" class="ac-list__link">
+              <img v-if="option.content_type.icon" :src="option.content_type.icon" :alt="option.title" class="ac-thumbnail" />
+              <!-- .ac-content -->
+              <div class="ac-content">
+                <h3 class="ac-title" v-if="option.title">{{option.title}}</h3>
+                <div class="ac-description" v-if="option.description">{{option.description}}</div>
+              </div>
+              <!-- END:.ac-content -->
+            </router-link>
           </li>
           <li>
             <button type="button" class="ac-blocktitle ac-more">+ See more</button>
@@ -139,12 +139,6 @@ export default {
         return
       }
       this.highlightedPosition = (this.highlightedPosition + 1) % this.options.length
-    },
-    select () {
-      const selectedOption = this.options[this.highlightedPosition]
-      this.searchQuery = selectedOption.title
-      this.close()
-      this.$emit('selected', selectedOption)
     },
     close () {
       this.isOpen = false
