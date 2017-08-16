0000d44bb2adf13525532491d2564364cf2b9896
Vocter <ameliepoulain-92@mail.ru>
2017-08-16 16:05:56 +0300

    add new api

--- src/api/index.js
+++ src/api/index.js
@@ -9,6 +9,6 @@ const api = {}
 export default api
 
 api.timeout = { timeout: 20000 }
-api.serverURL = 'http://192.168.0.85/web/app.php'
+api.serverURL = 'http://13.59.74.76'
 
 api.requestToServer = (urlEnd, type, payload = {}) => Vue.http[type](api.serverURL + urlEnd, payload, api.timeout)
