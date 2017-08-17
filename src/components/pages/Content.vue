cb97d6b05d0e655de46d45dee855e15f7ff6e803
Vocter <ameliepoulain-92@mail.ru>
2017-08-17 12:54:30 +0300

    add type

--- src/components/pages/Content.vue
+++ src/components/pages/Content.vue
@@ -2,7 +2,8 @@
 	<!-- .content -->
 	<div class="content">
 		<h1 class="content-title" v-if="title">{{title}}</h1>
-		<span class="content-type">{{type}}</span>
+		<span class="content-type" v-if="type">{{type}}</span>
+		<div class="content-description" v-if="description">{{description}}</div>
 	</div>
 	<!-- END:.content -->
 </template>
@@ -30,13 +31,18 @@ export default {
       if (this.contentItem) {
         return this.contentItem.content_type.type
       }
+    },
+    description () {
+      if (this.selectedItem) {
+        return this.contentItem.content
+      }
     }
   },
   methods: {
     fetchContent () {
       this.contentItem = null
       this.$http.get(`${api.serverURL}/api/v1/content/${this.id}`)
-      .then((res) => { this.contentItem = res.body })
+      .then((res) => { this.contentItem = res.body; console.log(res.body) })
     }
   },
   mounted () {
