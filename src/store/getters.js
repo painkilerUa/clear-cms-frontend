9dd6fac71d14dd09226fdf59728bb661a543862a
Vocter <ameliepoulain-92@mail.ru>
2017-08-17 09:28:39 +0300

    remove byid content

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,9 +2,6 @@ export const content = state => state.content
 export const types = state => state.types
 export const tags = state => state.tags
 
-// content
-export const getContentById = (state, getters) => (id) => state.content.items.find(item => item.id === id)
-
 // types
 export const getContentTypeTitles = (state, getters) => state.types.map(item => item.type)
 export const getContentTypeIdByTitle = (state, geters) => (type) => state.types.find(item => item.type === type).id
