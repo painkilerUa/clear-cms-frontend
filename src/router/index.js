6696c06894d347f75bacb90deaaae15e65318b17
Vocter <ameliepoulain-92@mail.ru>
2017-08-17 15:06:45 +0300

    add scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -35,5 +35,8 @@ export default new Router({
       component: Error
     },
     {path: '*', redirect: '/error'}
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
