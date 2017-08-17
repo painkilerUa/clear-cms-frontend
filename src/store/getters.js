e40c219f89b9f67f92bb8119c85007a765865b2f
Vocter <ameliepoulain-92@mail.ru>
2017-08-17 09:08:06 +0300

    add getter getcontentById

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,9 @@ export const content = state => state.content
 export const types = state => state.types
 export const tags = state => state.tags
 
+// content
+export const getContentById = (state, getters) => (id) => state.content.items.find(item => item.id === id)
+
 // types
 export const getContentTypeTitles = (state, getters) => state.types.map(item => item.type)
 export const getContentTypeIdByTitle = (state, geters) => (type) => state.types.find(item => item.type === type).id
