5d8566093a1dc2982b23ef5ec1f83ef3671e4e43
Vocter <ameliepoulain-92@mail.ru>
2017-08-18 15:29:32 +0300

    add store urls

--- src/api/index.js
+++ src/api/index.js
@@ -11,4 +11,12 @@ export default api
 api.timeout = { timeout: 20000 }
 api.serverURL = 'http://13.59.74.76'
 
+api.URLS = {
+  search: `${api.serverURL}/search?_format=json`,
+  login: `${api.serverURL}/api/v1/login`,
+  register: `${api.serverURL}/api/v1/register`,
+  contentTypes: '/api/v1/contentTypes',
+  tags: '/api/v1/tags'
+}
+
 api.requestToServer = (urlEnd, type, payload = {}) => Vue.http[type](api.serverURL + urlEnd, payload, api.timeout)
