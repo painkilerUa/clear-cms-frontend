03b65e4a7225731de0d316d8f520c094f1a8363a
Vocter <ameliepoulain-92@mail.ru>
2017-08-19 13:06:05 +0300

    remove unnes

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,10 +4,8 @@ export const getTypes = ({ commit }, payload) => {
   const urlEnd = api.URLS.contentTypes
   const type = 'get'
   api.requestToServer(urlEnd, type, payload)
-    .then((res) => {
-      commit('setTypes', res.body.items)
-    })
-    .catch((err) => console.error(err))
+  .then((res) => { commit('setTypes', res.body.items) })
+  .catch((err) => console.error(err))
 }
 
 export const getTags = ({ commit }, payload) => {
