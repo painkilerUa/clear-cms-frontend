fd628d6f18388dc91c6ab1b6c432fc2016a3dbde
Vocter <ameliepoulain-92@mail.ru>
2017-08-21 17:32:41 +0300

    add items

--- src/components/pages/AdminManagement.vue
+++ src/components/pages/AdminManagement.vue
@@ -2,6 +2,13 @@
 	<!-- .admin-management -->
 	<div class="admin-management">
 		<h1>Admin Management</h1>
+		<!-- admin-actions -->
+		<ul class="admin-actions">
+			<li class="admin-actions__item">Action1</li>
+			<li class="admin-actions__item">Action2</li>
+			<li class="admin-actions__item">Action3</li>
+		</ul>
+		<!-- END:admin-actions -->
       <v-select
       placeholder="Topic"
       value.sync="selectedTags"
