c827733d877104db192b795f6c477fae97f23e58
Vocter <ameliepoulain-92@mail.ru>
2017-08-21 17:28:38 +0300

    add select

--- src/components/pages/AdminManagement.vue
+++ src/components/pages/AdminManagement.vue
@@ -2,6 +2,12 @@
 	<!-- .admin-management -->
 	<div class="admin-management">
 		<h1>Admin Management</h1>
+      <v-select
+      placeholder="Topic"
+      value.sync="selectedTags"
+      :multiple="true"
+      :on-change="selectTags"
+      :options="getTagTitles" />
 	</div>
 	<!-- END:.admin-management -->
 </template>
