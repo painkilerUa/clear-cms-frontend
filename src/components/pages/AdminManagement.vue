5fb76f044b7f685048bf8b988513900f077a0ec2
Vocter <ameliepoulain-92@mail.ru>
2017-08-21 16:27:43 +0300

    add admin management com

---
+++ src/components/pages/AdminManagement.vue
@@ -0,0 +1,13 @@
+<template>
+	<!-- .admin-management -->
+	<div class="admin-management">
+		<h1>Admin Management</h1>
+	</div>
+	<!-- END:.admin-management -->
+</template>
+
+<script>
+export default {
+  name: 'admin-management'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import Router from 'vue-router'
 import Login from '@/components/pages/Login'
 import Register from '@/components/pages/Register'
 import ForgotPassword from '@/components/pages/ForgotPassword'
-import Error from '@/components/pages/Error'
+import AdminManagement from '@/components/pages/AdminManagement'
 import Content from '@/components/pages/Content'
+import Error from '@/components/pages/Error'
 
 Vue.use(Router)
 
@@ -29,6 +30,11 @@ export default new Router({
       path: '/forgot-password',
       name: 'forgot-password',
       component: ForgotPassword
+    },
+    {
+      path: '/admin-management',
+      name: 'admin-management',
+      component: AdminManagement
     },
     {
       path: '/content/:id',
