9efa47ad14f3518cc1c8d2d6d2a49bea14f50026
Vocter <ameliepoulain-92@mail.ru>
2017-08-21 15:36:45 +0300

    changes

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,7 @@ export const setTypes = (state, types) => { state.types = types }
 export const setTags = (state, tags) => { state.tags = tags }
 
 // auth
-export const authLogin = (state) => { state.auth.pending = true }
+export const authLogin = state => { state.auth.pending = true }
 export const authLoginSuccess = (state) => { state.auth.isLoggedIn = true; state.auth.pending = false }
 export const authLoginSetUsername = (state, name) => { state.auth.username = name }
-export const authLogout = (state) => { state.auth.isLoggedIn = false }
+export const authLogout = state => { state.auth.isLoggedIn = false }
