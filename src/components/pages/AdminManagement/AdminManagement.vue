6175f10837c33a0d499eae2c865c0fef69a62d0d
Vocter <ameliepoulain-92@mail.ru>
2017-08-22 11:40:09 +0300

    new comp admmanag

--- src/components/pages/AdminManagement/AdminManagement.vue
+++ src/components/pages/AdminManagement/AdminManagement.vue
@@ -2,6 +2,20 @@
 	<!-- .admin-management -->
 	<div class="admin-management">
 		<h1>Admin Management</h1>
+    <!-- .actions-table -->
+    <div class="actions-table">
+      <!-- .actions-heading -->
+      <div class="actions-heading">
+      	<div class="actions-cell actions-heading__cell">
+      		<span class="actions-cell__title">Admin</span>
+      		<input type="checkbox" />
+      	</div>
+      	<div class="actions-cell actions-heading__cell">User</div>
+      	<div class="actions-cell actions-heading__cell">Editor</div>
+      </div>
+      <!-- END:.actions-heading -->
+    </div>
+    <!-- END:.actions-table -->
 	</div>
 	<!-- END:.admin-management -->
 </template>
@@ -22,3 +36,5 @@ export default {
   }
 }
 </script>
+
+<style src="@/assets/scss/components/admin-management.scss" lang="scss" scoped />
