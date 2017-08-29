daf30ba3733b05b1c22c77e5ed8922e3bb835b4f
Vocter <ameliepoulain-92@mail.ru>
2017-08-29 16:21:12 +0300

    add inputwrap

--- src/components/pages/ArticlesList.vue
+++ src/components/pages/ArticlesList.vue
@@ -1,14 +1,23 @@
 <template>
 	<!-- .articles-list -->
 	<div class="articles-list">
-   <!-- .articles-list-actions -->
-    <div class="articles-list-actions">
-      <v-select
-      placeholder="Bulk action"
-      v-model="selectedAction"
-      :options="getArticleListActions" />
+   <!-- .articles-list-heading -->
+   <div class="articles-list-heading">
+      <!-- .articles-list-actions -->
+      <div class="articles-list-actions">
+        <!-- .articles-list-inputwrap -->
+        <div class="articles-list-inputwrap">
+        	<input type="checkbox" />
+        </div>
+        <!-- END:.articles-list-inputwrap -->
+        <v-select
+        placeholder="Bulk action"
+        v-model="selectedAction"
+        :options="getArticleListActions" />
+      </div>
+      <!-- END:.articles-list-actions -->
     </div>
-    <!-- END:.articles-list-actions --> 
+   <!-- END:.articles-list-heading -->
   </div>
 	<!-- END:.articles-list -->
 </template>
@@ -18,7 +27,7 @@ export default {
   name: 'articles-list',
   data () {
     return {
-      getArticleListActions: [],
+      getArticleListActions: ['Action 1', 'Action 2', 'Action 3'],
       selectedAction: null
     }
   }
