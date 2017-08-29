9486c09f24d7f31c33a33bfbf36bc5f15824cf43
Vocter <ameliepoulain-92@mail.ru>
2017-08-29 16:41:38 +0300

    add new

--- src/components/pages/ArticlesList.vue
+++ src/components/pages/ArticlesList.vue
@@ -16,6 +16,20 @@
         :options="getArticleListActions" />
       </div>
       <!-- END:.articles-list-actions -->
+      <!-- .articles-list-statusbar -->
+      <div class="articles-list-statusbar">
+      	<!-- .articles-list-statusbar__contains -->
+      	<div class="articles-list-statusbar__contains articles-list-statusbar__block">Library contains 33.456 Articles</div>
+      	<!-- END:.articles-list-statusbar__contains -->
+      	<!-- .articles-list-statuses -->
+      	<ul class="articles-list-statuses articles-list-statusbar__block">
+      		<li class="articles-list-statuses__item articles-list-statuses__item--published">12567 Published</li>
+      		<li class="articles-list-statuses__item articles-list-statuses__item--archived">12567 Archived</li>
+      		<li class="articles-list-statuses__item articles-list-statuses__item--draft">12567 Draft</li>
+      	</ul>
+      	<!-- END:.articles-list-statuses -->
+      </div>
+      <!-- END:.articles-list-statusbar -->
     </div>
    <!-- END:.articles-list-heading -->
   </div>
