c27c40a7fb310332bf90b7b127e350f13c709d4b
Vocter <ameliepoulain-92@mail.ru>
2017-08-29 15:50:14 +0300

    add articlesList component

---
+++ src/components/pages/ArticlesList.vue
@@ -0,0 +1,16 @@
+<template>
+	<!-- .articles-list -->
+	<div class="articles-list">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit dignissimos quae iste saepe quaerat nesciunt doloribus at maiores dolorem atque aspernatur, magnam temporibus quod eligendi consequatur, perspiciatis dolore ea similique?
+  </div>
+	<!-- END:.articles-list -->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
