9061bff41b0212fa1f0244587122ebeed176433c
Vocter <ameliepoulain-92@mail.ru>
2017-08-30 14:49:21 +0300

    add new article

---
+++ src/components/pages/AddArticle.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- add-article -->
+  <div class="add-article">
+    <h1>Add new article</h1>
+  </div>
+  <!-- END:add-article -->
+</template>
+
+<script>
+export default {
+  name: 'add-article',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style src='@/assets/scss/components/add-article.scss' lang='scss' scoped />
