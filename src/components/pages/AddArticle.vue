dcbcb8ba1563d8cccb482323d6242914db0f0fd2
Vocter <ameliepoulain-92@mail.ru>
2017-08-30 15:45:42 +0300

    add articles increment

--- src/components/pages/AddArticle.vue
+++ src/components/pages/AddArticle.vue
@@ -1,7 +1,19 @@
 <template>
   <!-- add-article -->
   <div class="add-article">
-    <h1>Add new article</h1>
+    <h1 class="add-article-title">Add new article</h1>
+    <!-- .add-article-wrapper -->
+    <div class="add-article-wrapper">
+      <section class="add-article-section">
+        <h2 class="add-article-section__title">Article details</h2>
+        <!-- .add-article-details -->
+        <div class="add-article-details">
+          
+        </div>
+        <!-- END:.add-article-details -->
+      </section>
+    </div>
+    <!-- END:.add-article-wrapper -->
   </div>
   <!-- END:add-article -->
 </template>
