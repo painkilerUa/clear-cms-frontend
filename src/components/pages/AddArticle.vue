a4e66810322e9e89b39881c5350a0b530912f849
Vocter <ameliepoulain-92@mail.ru>
2017-08-31 11:19:03 +0300

    add append method

--- src/components/pages/AddArticle.vue
+++ src/components/pages/AddArticle.vue
@@ -104,69 +104,85 @@
           </section>
           <section class="add-article-section">
             <h2 class="add-article-section__title">Article video</h2>
-            <!-- .form-element -->
-            <div class="form-element">
-              <label for="addVideoUrl" class="form-label">Video URL</label>
-              <input
-              id="addVideoUrl"
-              type="text"
-              placeholder="Insert video link here"
-              class="form-control" />
-            </div>
-            <!-- END:.form-element -->
-            <!-- .form-element -->
-            <div class="form-element">
-              <label for="addTextarea" class="form-label">Transcript for video</label>
-              <textarea
-              id="addTextarea"
-              placeholder="Transcript for video"
-              class="form-control"
-              cols="30"
-              rows="5">
-              </textarea>
-              <button type="button" class="add-article-btn alignright">Add video</button>
-            </div>
-            <!-- END:.form-element -->
-          </section>
-          <section class="add-article-section">
-            <h2 class="add-article-section__title">Article resources</h2>
-            <!-- .form-group -->
-            <div class="form-group">
+            <!-- .form-elements -->
+            <div class="form-elements" v-for="n in videoRange">
               <!-- .form-element -->
-              <div class="form-element form-element--half">
-                <label for="addTitle" class="form-label">Upload resource</label>
+              <div class="form-element">
+                <label for="addVideoUrl" class="form-label">Video URL</label>
                 <input
-                id="addTitle"
-                type="file"
-                placeholder="Type in to add title..."
+                id="addVideoUrl"
+                type="text"
+                placeholder="Insert video link here"
                 class="form-control" />
               </div>
               <!-- END:.form-element -->
               <!-- .form-element -->
-              <div class="form-element form-element--half">
-                <label for="addTitle" class="form-label">Insert resource URL</label>
-                <input
-                id="addTitle"
-                type="url"
-                placeholder="Insert resource link here"
-                class="form-control" />
-              </div>
-              <!-- END:.form-element -->
-              <!-- .form-element -->
-              <div class="form-element form-element--full">
-                <label for="addTextarea" class="form-label">Transcript for resource</label>
+              <div class="form-element">
+                <label for="addTextarea" class="form-label">Transcript for video</label>
                 <textarea
                 id="addTextarea"
-                placeholder="Transcript for resource"
+                placeholder="Transcript for video"
                 class="form-control"
                 cols="30"
                 rows="5">
                 </textarea>
               </div>
               <!-- END:.form-element -->
-            </div>
-            <!-- END:.form-group -->
-            <button type="button" class="add-article-btn alignright">Add resource</button>
+              </div>
+            <!-- END:.form-elements -->
+            <button
+              type="button"
+              @click="addFormElement('video')"
+              class="add-article-btn alignright">Add video
+            </button>
+          </section>
+          <section class="add-article-section">
+            <h2 class="add-article-section__title">Article resources</h2>
+            <!-- .form-elements -->
+            <div class="form-elements" v-for="n in resourceRange">
+              <!-- .form-group -->
+              <div class="form-group">
+                <!-- .form-element -->
+                <div class="form-element form-element--half">
+                  <label for="addTitle" class="form-label">Upload resource</label>
+                  <input
+                  id="addTitle"
+                  type="file"
+                  placeholder="Type in to add title..."
+                  class="form-control" />
+                </div>
+                <!-- END:.form-element -->
+                <!-- .form-element -->
+                <div class="form-element form-element--half">
+                  <label for="addTitle" class="form-label">Insert resource URL</label>
+                  <input
+                  id="addTitle"
+                  type="url"
+                  placeholder="Insert resource link here"
+                  class="form-control" />
+                </div>
+                <!-- END:.form-element -->
+                <!-- .form-element -->
+                <div class="form-element form-element--full">
+                  <label for="addTextarea" class="form-label">Transcript for resource</label>
+                  <textarea
+                  id="addTextarea"
+                  placeholder="Transcript for resource"
+                  class="form-control"
+                  cols="30"
+                  rows="5">
+                  </textarea>
+                </div>
+                <!-- END:.form-element -->
+              </div>
+              <!-- END:.form-group -->
+              </div>
+              <!-- END:.form-elements -->
+            <button
+              type="button"
+              @click="addFormElement('resource')"
+              class="add-article-btn alignright">Add resource
+            </button>
           </section>
         </div>
         <!-- END:.add-article-sections -->
@@ -202,7 +218,15 @@ export default {
     return {
       formInfo: {
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At voluptas tempore error quis recusandae perferendis aliquam, dolore alias, commodi natus labore dolorum obcaecati, voluptatibus sunt rem atque iusto odit, facere vel similique impedit. Qui, voluptas voluptatibus suscipit modi omnis harum, deserunt libero maxime natus, ad veniam inventore rerum velit numquam? Est obcaecati dolores laudantium dignissimos, saepe atque. Porro a dolorem tempora, ad vero rerum at. Nostrum provident sapiente praesentium atque, expedita alias, hic perspiciatis quae sed, aliquam beatae, rem optio magni perferendis quo numquam deleniti magnam. Tenetur vero expedita, aliquam, quis fugit laborum quisquam doloremque asperiores eligendi dolores facere maiores.'
-      }
+      },
+      videoRange: 1,
+      resourceRange: 1
+    }
+  },
+  methods: {
+    addFormElement (type) {
+      let typeRange = `${type}Range`
+      this[typeRange] += 1
     }
   },
   components: {
