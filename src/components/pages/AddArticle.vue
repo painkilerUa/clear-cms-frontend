9c1cb6d489d02440a50e8caf17c3a817a5817ef2
Vocter <ameliepoulain-92@mail.ru>
2017-08-31 17:35:02 +0300

    content validataion

--- src/components/pages/AddArticle.vue
+++ src/components/pages/AddArticle.vue
@@ -283,7 +283,7 @@ export default {
         access: [],
         category: null,
         companySpecific: null,
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At voluptas tempore error quis recusandae perferendis aliquam, dolore alias, commodi natus labore dolorum obcaecati, voluptatibus sunt rem atque iusto odit, facere vel similique impedit. Qui, voluptas voluptatibus suscipit modi omnis harum, deserunt libero maxime natus, ad veniam inventore rerum velit numquam? Est obcaecati dolores laudantium dignissimos, saepe atque. Porro a dolorem tempora, ad vero rerum at. Nostrum provident sapiente praesentium atque, expedita alias, hic perspiciatis quae sed, aliquam beatae, rem optio magni perferendis quo numquam deleniti magnam. Tenetur vero expedita, aliquam, quis fugit laborum quisquam doloremque asperiores eligendi dolores facere maiores.'
+        content: ''
       },
       languages: ['English (UK)', 'English (US)'],
       types: [],
