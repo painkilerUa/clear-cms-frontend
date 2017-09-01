752b934c258132091c905f7ad813a1554e8b2c7b
Vocter <ameliepoulain-92@mail.ru>
2017-09-01 11:48:01 +0300

    add new instance

--- src/components/pages/ArticleAddData.vue
+++ src/components/pages/ArticleAddData.vue
@@ -1,11 +1,3 @@
-<template>
-	<!-- .article-add-data -->
-	<div class="article-add-data">
-		
-	</div>
-	<!-- END:.article-add-data -->
-</template>
-
 <script>
 export default {
   name: 'article-add-data',
