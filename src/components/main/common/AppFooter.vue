a426a8ee42074a8ec203629e1ccad1db1c446062
Arthur <ameliepoulain-92@mail.ru>
2017-10-06 14:04:23 +0300

    update search icon magnifier

--- src/components/main/common/AppFooter.vue
+++ src/components/main/common/AppFooter.vue
@@ -1,27 +1,41 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3 address col-sm-4">
-          The Clear Company, The Base,<br>
-          20 Dallam Lane, Warrington,<br>
-          Cheshire, WA2 7NG<br>
-          <span>0845 2930963</span>
+  <footer class="front-footer">
+    <!-- .front-footer-top -->
+    <div class="front-footer-top">
+      <!-- .myContainer -->
+      <div class="myContainer">
+        <!-- .front-footer-top__inner -->
+        <div class="front-footer-top__inner">
+          <!-- .front-footer-info -->
+          <div class="front-footer-info">
+            <p>The Clear Company, The Base, <br /> 20 Dallam Lane, Warrington, <br /> Cheshire, WA2 7NG</p>
+            <a href="tel:08452930963" class="front-footer-phone">0845 2930963</a>
+          </div>
+          <!-- END:.front-footer-info -->
+          <!-- .front-footer-social -->
+          <div class="front-footer-social">
+            <h4 class="front-footer-social__title">Join us on...</h4>
+            ul.front-footer-social-list>li.-item>a.-link>img
+          </div>
+          <!-- END:.front-footer-social -->
         </div>
-        <div class="col-md-5 col-md-offset-4 block-icon col-sm-5 col-sm-offset-3">
-          <p>Join us on...</p>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/youtube.png" alt="youtube" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/picasso.png" alt="picasso" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/facebook.png" alt="facebook" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/twitter.png" alt="twitter" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/google_plus.png" alt="google_plus" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/in.png" alt="in" /></a>
-          <a href="#"><img src="../../../assets/img/main/footer/icon/pinit.png" alt="pinit" /></a>
-        </div>
+        <!-- END:.front-footer-top__inner -->
       </div>
+      <!-- END:.myContainer -->
     </div>
-    <div class="line_bottom"></div>
-    <div class="container footer">
+    <!-- END:.front-footer-top -->
+    <!-- <div class="col-md-5 col-md-offset-4 block-icon col-sm-5 col-sm-offset-3">
+      <p>Join us on...</p>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/youtube.png" alt="youtube" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/picasso.png" alt="picasso" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/facebook.png" alt="facebook" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/twitter.png" alt="twitter" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/google_plus.png" alt="google_plus" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/in.png" alt="in" /></a>
+      <a href="#"><img src="../../../assets/img/main/footer/icon/pinit.png" alt="pinit" /></a>
+    </div> -->
+    <!-- <div class="line_bottom"></div> -->
+    <!-- <div class="container footer">
       <div class="row">
         <div class="col-md-12 terms">
           <a href="#">Terms and conditions</a>
@@ -29,7 +43,7 @@
           <a href="#">Accessibility section</a>
         </div>
       </div>
-    </div>
+    </div> -->
   </footer>
 </template>
 
