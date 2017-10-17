959444356be187bf0b94cf9ffb481106d03575d7
Andrew <kozhemyakin.andrej@gmail.com>
2017-10-17 16:28:50 +0300

    AskExperts_logo_block

--- src/components/main/AskExperts.vue
+++ src/components/main/AskExperts.vue
@@ -1,23 +1,126 @@
 <template>
-  <p>Ask experts</p>
+    <div class="wrapper">
+        <div class="picture"></div>
+        <div class="container">
+            <div class="form-container">
+                <form class="style-form">
+                    <p class="header-form">Ask an expert</p>
+                    <p class="message"><input class="header-form-text title" type="text" name="" value="Message title"></p>
+                    <p class="textfield"><input class="header-form-text textarea" type="text" name="" value=""></p>
+                    <p><input class="submit" type="submit" value="Submit you question"></p>
+                </form>
+            </div>
+
+            <div class="text-paragraf">
+                <p class="information">Dipidelenis qui omni omnihic tesequid erferuntium re ex et volorum rerum re rem quam nis dolum aut occullabo.
+                Min eos repel et asi re esciis maion re eicilia ecatur solorae. Acernatqui omnition pliti illoreperro est orum
+                rerum re rem quam nis dolum aut occasta ostaha d aga sjau yo.
+                </p>
+            </div>
+
+            <section class="client">
+                <div class="client-block flex-container">
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img src="../../assets/img/main/AbilityNet.png" alt="">
+                        </div>
+                        <h3 class="client-block-item-logo-name">Ability Net </h3>
+                        <div class="client-block-item-content flex-container">
+                            <p class="flex-block">Experts in Accessible Technology</p>
+                            <a class="client-block-item-logo-name-chat flex-block" href="">
+                                <div class="client-block-item-logo-name-chat-icon"> </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img src="../../assets/img/main/theClearCompany.png" alt="">
+                        </div>
+                        <h3 class="client-block-item-logo-name">Ability Net </h3>
+                        <div class="client-block-item-content flex-container">
+                            <p class="flex-block">Experts in Accessible Technology</p>
+                            <a class="client-block-item-logo-name-chat flex-block" href="">
+                                <div class="client-block-item-logo-name-chat-icon"> </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img class="client-block-item-logo-dwf" src="../../assets/img/main/dwf.png" alt="">
+                        </div>
+                        <h3 class="client-block-item-logo-name">Ability Net </h3>
+                        <div class="client-block-item-content flex-container">
+                            <p class="flex-block">Experts in Accessible Technology</p>
+                            <a class="client-block-item-logo-name-chat flex-block" href="">
+                                <div class="client-block-item-logo-name-chat-icon"> </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img class="gray rectangle" src="" alt="">
+                        </div>
+                        <h3 class="client-block-item-logo-name">Ability Net </h3>
+                        <div class="client-block-item-content flex-container">
+                            <p class="flex-block">Experts in Accessible Technology</p>
+                            <a class="client-block-item-logo-name-chat flex-block" href="">
+                                <div class="client-block-item-logo-name-chat-icon"> </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img class="gray rectangle" src="" alt="">
+                        </div>
+                            <h3 class="client-block-item-logo-name">Ability Net </h3>
+                            <div class="client-block-item-content flex-container">
+                                <p class="flex-block">Experts in Accessible Technology</p>
+                                    <a class="client-block-item-logo-name-chat flex-block" href="">
+                                        <div class="client-block-item-logo-name-chat-icon"> </div>
+                                    </a>
+                            </div>
+                    </div>
+
+                    <div class="client-block-item flex-block">
+                        <div class="client-block-item-logo">
+                            <img class="gray rectangle" src="" alt="">
+                        </div>
+                        <h3 class="client-block-item-logo-name">Ability Net </h3>
+                        <div class="client-block-item-content flex-container">
+                            <p class="flex-block">Experts in Accessible Technology</p>
+                            <a class="client-block-item-logo-name-chat flex-block" href="">
+                                <div class="client-block-item-logo-name-chat-icon"> </div>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+    </div>
 </template>
 
-<script>
-// import api from '@/api'
-
-export default {
-  name: 'AskExperts',
-  data () {
-    return {
-    }
-  },
-  computed: {
-  },
-  methods: {
-  },
-  mounted () {
-  }
-}
+<script  >
+//     import api from '@/api'
+//
+//    export default {
+//        name: 'AskExperts',
+//        data () {
+//        return {
+//        }
+//    },
+//    computed: {
+//    },
+//    methods: {
+//    },
+//    mounted () {
+//    }
+//    }
 </script>
 
 <style src="@/assets/scss/main/ask-experts.scss" lang="scss" scoped/>
